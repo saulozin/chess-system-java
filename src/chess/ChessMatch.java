778f777d2991a822462ff3d60efe1c8469574fe1
Saulo Maia <saulomaia2@gmail.com>
2022-11-30 09:47:02 -0400

    little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -30,12 +29,11 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
-		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 0));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 7));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+		
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 		
 	}
 
